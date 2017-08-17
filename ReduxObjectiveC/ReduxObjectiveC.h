5b81bbb0890d73c5c45264bec90e90d186613578
sriky <srikanth.kyatham@kaltiot.com>
2017-08-17 14:06:31 +0300

    Initial Commit

---
+++ ReduxObjectiveC/ReduxObjectiveC.h
@@ -0,0 +1,19 @@
+//
+//  ReduxObjectiveC.h
+//  ReduxObjectiveC
+//
+//  Created by SRIKANTH KYATHAM on 17/08/17.
+//  Copyright © 2017 SRIKANTH KYATHAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReduxObjectiveC.
+FOUNDATION_EXPORT double ReduxObjectiveCVersionNumber;
+
+//! Project version string for ReduxObjectiveC.
+FOUNDATION_EXPORT const unsigned char ReduxObjectiveCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReduxObjectiveC/PublicHeader.h>
+
+
