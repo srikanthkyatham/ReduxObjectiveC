f7b1e198f0226ff5b385a662c2e30e88620a8dac
sriky <srikanth.kyatham@kaltiot.com>
2017-08-18 14:32:20 +0300

    Adding public headers in framework header file

--- ReduxObjectiveC/ReduxObjectiveC.h
+++ ReduxObjectiveC/ReduxObjectiveC.h
@@ -15,5 +15,7 @@ FOUNDATION_EXPORT double ReduxObjectiveCVersionNumber;
 FOUNDATION_EXPORT const unsigned char ReduxObjectiveCVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ReduxObjectiveC/PublicHeader.h>
-
+#import <ReduxObjectiveC/Store.h>
+#import <ReduxObjectiveC/Action.h>
+#import <ReduxObjectiveC/Reducer.h>
 
